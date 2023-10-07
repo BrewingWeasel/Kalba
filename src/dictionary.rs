786e56cc43d269cad78b36f0557b891f4099e11a
BrewingWeasel <weeaseled@gmail.com>
2023-10-07 18:26:20 -0400

    feat: allow using stardict

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,18 +1,54 @@
+use scraper::{Html, Selector};
+use serde::Deserialize;
+use serde_derive::Serialize;
 use std::fs;
 
 use crate::settings::Dictionary;
 
-fn get_def(lemma: &str, file: &str) -> String {
-    if let Ok(lines) = fs::read_to_string(file) {
-        for line in lines.lines() {
-            let (word, def) = line.split_once(":\t").unwrap();
-            if word == lemma {
-                return def.to_owned();
+#[derive(Serialize, Deserialize, PartialEq)]
+pub enum DictFileType {
+    TextSplitAt(String),
+    StarDict,
+}
+
+fn get_def(lemma: &str, file: &str, dict_type: &DictFileType) -> String {
+    match dict_type {
+        DictFileType::StarDict => {
+            let mut dict = stardict::no_cache(file).unwrap();
+
+            let mut def = String::new();
+            if let Ok(response) = stardict::StarDict::lookup(&mut dict, lemma) {
+                for word in &response.unwrap() {
+                    if word.word != lemma {
+                        continue;
+                    }
+                    for i in &word.segments {
+                        let fragment = Html::parse_fragment(&i.text);
+                        let selector = Selector::parse("li").unwrap();
+
+                        for element in fragment.select(&selector) {
+                            assert_eq!("li", element.value().name());
+                            def.push_str(&element.text().fold(String::new(), |acc, n| acc + n));
+                            def.push('\n')
+                        }
+                    }
+                }
+            }
+            def
+        }
+        DictFileType::TextSplitAt(delim) => {
+            if let Ok(lines) = fs::read_to_string(file) {
+                for line in lines.lines() {
+                    let (word, def) = line.split_once(delim).unwrap();
+                    if word == lemma {
+                        return def.to_owned();
+                    }
+                }
+                String::from("unknown")
+            } else {
+                String::from("failed to load dictionary")
             }
         }
-        String::from("unknown")
-    } else {
-        String::from("failed to load dictionary")
     }
 }
 
@@ -28,7 +64,7 @@ pub fn get_defs(lemma: &str, dicts: &Vec<Dictionary>) -> Vec<String> {
     let mut defs = Vec::new();
     for dict in dicts {
         let def = match dict {
-            Dictionary::File(f) => get_def(lemma, f),
+            Dictionary::File(f, dict_type) => get_def(lemma, f, dict_type),
             Dictionary::Url(url) => get_def_url(lemma, url),
         };
         defs.push(def);
--- src/settings.rs
+++ src/settings.rs
@@ -4,6 +4,8 @@ use std::fs;
 
 use eframe::egui::{self, Context, TextEdit};
 
+use crate::dictionary::DictFileType;
+
 #[derive(Deserialize, Serialize)]
 pub struct Settings {
     pub model: String,
@@ -14,8 +16,8 @@ pub struct Settings {
 #[derive(Serialize, Deserialize, PartialEq)]
 #[serde(tag = "t", content = "c")]
 pub enum Dictionary {
-    File(String), // Eventually these will have specific things
-    Url(String),  // TODO: implement url
+    File(String, DictFileType), // Eventually these will have specific things
+    Url(String),                // TODO: implement url
 }
 
 impl Settings {
@@ -61,28 +63,65 @@ impl Settings {
                 ui.horizontal(|ui| {
                     ui.label("Dictionaries:");
                     if ui.button("new dict").clicked() {
-                        self.dicts.push(Dictionary::File(String::new()))
+                        self.dicts
+                            .push(Dictionary::File(String::new(), DictFileType::StarDict))
                     }
                 });
                 for (i, dict) in self.dicts.iter_mut().enumerate() {
                     let selected = match dict {
-                        Dictionary::File(_) => "File",
+                        Dictionary::File(_, _) => "File",
                         Dictionary::Url(_) => "URL",
                     };
                     ui.horizontal(|ui| {
                         egui::ComboBox::from_label(format!("Dict {i}"))
                             .selected_text(selected)
                             .show_ui(ui, |ui| {
-                                ui.selectable_value(dict, Dictionary::File(String::new()), "File");
+                                ui.selectable_value(
+                                    dict,
+                                    Dictionary::File(String::new(), DictFileType::StarDict),
+                                    "Local Dictionary",
+                                );
                                 ui.selectable_value(dict, Dictionary::Url(String::new()), "URL");
                             });
                         ui.add_space(5.0);
                         match dict {
-                            Dictionary::File(file) => {
+                            Dictionary::File(file, dict_type) => {
                                 ui.add(TextEdit::singleline(file).desired_width(400.0));
+
+                                let selected = match dict_type {
+                                    DictFileType::StarDict => "StarDict",
+                                    DictFileType::TextSplitAt(_) => "Custom",
+                                };
+
+                                egui::ComboBox::from_id_source(i)
+                                    .selected_text(selected)
+                                    .show_ui(ui, |ui| {
+                                        ui.selectable_value(
+                                            dict_type,
+                                            DictFileType::StarDict,
+                                            "StarDict",
+                                        );
+                                        ui.selectable_value(
+                                            dict_type,
+                                            DictFileType::TextSplitAt(String::from("=")),
+                                            "Custom Type",
+                                        );
+                                    });
+
+                                if let DictFileType::TextSplitAt(delimiter) = dict_type {
+                                    ui.add(TextEdit::singleline(delimiter).desired_width(40.0));
+                                }
+
                                 if ui.button("select file").clicked() {
-                                    if let Some(selected_file) = rfd::FileDialog::new().pick_file()
-                                    {
+                                    let file_picker = match dict_type {
+                                        DictFileType::TextSplitAt(_) => {
+                                            rfd::FileDialog::new().pick_file()
+                                        }
+                                        DictFileType::StarDict => rfd::FileDialog::new()
+                                            .add_filter("StarDict", &["ifo"])
+                                            .pick_file(),
+                                    };
+                                    if let Some(selected_file) = file_picker {
                                         *file = selected_file.to_str().unwrap().to_owned();
                                     }
                                 }
