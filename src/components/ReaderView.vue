1fd61fa918d124a7bdcff1e3c1985ae4c7f90780
BrewingWeasel <weeaseled@gmail.com>
2024-01-18 23:36:53 -0800

    feat: send sentences

--- src/components/ReaderView.vue
+++ src/components/ReaderView.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
-import { useRouter } from "vue-router";
-import { Button } from '@/components/ui/button'
+// import { useRouter } from "vue-router";
+// import { Button } from '@/components/ui/button'
+import Word from "@/components/Word.vue";
 
-const words = ref([{ display: "Labas", }, "kaip", "gyveni"]);
-const router = useRouter();
+const sentence = "Noriu tapti programuotoju.";
+const words = ref([]);
+const selected_word = ref("");
+set_words();
+
+
+async function set_words() {
+  words.value = await invoke("parse_text", { sent: sentence });
+}
+
+function handle_word_selected(word) {
+  selected_word.value = word;
+}
 
 </script>
 
 <template>
-  <v-for="word in words">
+  <h1>Reader</h1>
+  <h2>Selected word</h2>
+  <Word v-for="word in words" :word="word" @selected="handle_word_selected" />
 </template>
---
+++ src/components/Word.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
+const props = defineProps(['word'])
+
+const rating = `rating-${props.word.rating}`
+
+function set_selected() {
+  console.log('set_selected')
+  this.$emit('selected', props.word)
+}
+</script>
+
+<template @click.native="set_selected">
+  <span v-if="props.word.clickable">&nbsp;</span>
+  <HoverCard :class="rating">
+    <HoverCardTrigger>{{ props.word.text }}</HoverCardTrigger>
+    <HoverCardContent>
+      {{ props.word.lemma }}
+      {{ props.word.morph }}
+    </HoverCardContent>
+  </HoverCard>
+</template>
