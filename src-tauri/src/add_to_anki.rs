4069246031997da9363c9651f93eaaff6292d72b
Finn Brewer <weeaseled@gmail.com>
2024-06-21 17:23:06 -0700

    test: fix anki tests not compiling

--- src-tauri/src/add_to_anki.rs
+++ src-tauri/src/add_to_anki.rs
@@ -83,7 +83,15 @@ mod test {
     #[test]
     fn default_settings_no_defs() {
         let settings = Settings::default();
-        let args = get_json("mmm", "word", Vec::new(), &settings).unwrap();
+        let details = ExportDetails {
+            word: "word",
+            sentence: "mmm",
+            deck: "Default",
+            model: "Basic",
+            defs: Vec::new(),
+            fields: HashMap::from([("Front", "{sentence}"), ("Back", "{word}:")]),
+        };
+        let args = get_json(details).unwrap();
         let params = args.get("params").unwrap();
         let note = params.get("note").unwrap();
         assert_eq!(
@@ -103,17 +111,20 @@ mod test {
     #[test]
     fn default_settings_defs() {
         let settings = Settings::default();
-        let args = get_json(
-            "sent:2",
-            "word",
-            vec![
+
+        let details = ExportDetails {
+            word: "word",
+            sentence: "sent:2",
+            deck: "Default",
+            model: "Basic",
+            defs: vec![
                 String::from("def1"),
                 String::from("def2"),
                 String::from("def3"),
             ],
-            &settings,
-        )
-        .unwrap();
+            fields: HashMap::from([("Front", "{sentence}"), ("Back", "{word}:{def}")]),
+        };
+        let args = get_json(details).unwrap();
         let params = args.get("params").unwrap();
         let note = params.get("note").unwrap();
         assert_eq!(
@@ -135,17 +146,20 @@ def3"})
 
     #[test]
     fn custom_fields_and_deck_defs_1() {
-        let settings = Settings {
-            deck: String::from("deck"),
-            note_type: String::from("note"),
-            note_fields: HashMap::from([
-                (String::from("sentence"), String::from("{sent}[{word}]")),
-                (String::from("word"), String::from("{word}")),
-                (String::from("def"), String::from("{def}")),
+        let details = ExportDetails {
+            word: "word",
+            sentence: "sent",
+            deck: "deck",
+            model: "note",
+            defs: vec![String::from("def1")],
+            fields: HashMap::from([
+                ("sentence", "{sentence}[{word}]"),
+                ("word", "{word}"),
+                ("def", "{def}"),
             ]),
-            ..Default::default()
         };
-        let args = get_json("sent", "word", vec![String::from("def1")], &settings).unwrap();
+
+        let args = get_json(details).unwrap();
         let params = args.get("params").unwrap();
         let note = params.get("note").unwrap();
         assert_eq!(
@@ -164,18 +178,17 @@ def3"})
 
     #[test]
     fn custom_fields_and_deck_same_field_twice() {
-        let settings = Settings {
-            deck: String::from("deck"),
-            note_type: String::from("note"),
-            note_fields: HashMap::from([
-                (String::from("sentence"), String::from("{sent}")),
-                (String::from("sentence"), String::from("{word}")),
-            ]),
-            ..Default::default()
+        let details = ExportDetails {
+            word: "word",
+            sentence: "sent",
+            deck: "deck",
+            model: "note",
+            defs: Vec::new(),
+            fields: HashMap::from([("sentence", "{sentence}"), ("sentence", "{sentence}")]),
         };
-        let args = get_json("sent", "word", Vec::new(), &settings).unwrap();
+        let args = get_json(details).unwrap();
         let params = args.get("params").unwrap();
         let note = params.get("note").unwrap();
-        assert_eq!(note.get("fields").unwrap(), &json!({"sentence": "word"}));
+        assert_eq!(note.get("fields").unwrap(), &json!({"sentence": "sent"}));
     }
 }
