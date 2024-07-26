beea5c9b74018190632d12061a4785a6f019511d
Finn Brewer <weeaseled@gmail.com>
2024-07-26 14:04:35 -0700

    feat: add tooltips for word knowledge buttons

---
+++ src/components/BetterTooltip.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+defineProps<{
+  tooltip: string;
+}>();
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent side="top"> {{ tooltip }} </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
--- src/components/RatingButtons.vue
+++ src/components/RatingButtons.vue
@@ -1,38 +1,51 @@
 <script setup lang="ts">
 import { Button } from "@/components/ui/button";
+import BetterTooltip from "./BetterTooltip.vue";
 </script>
 
 <template>
   <div class="flex gap-3 justify-center">
-    <Button
-      size="icon"
-      class="bg-rose-600 hover:bg-rose-500 size-7"
-      @click="$emit('change-rating', 0)"
-    ></Button>
-    <Button
-      size="icon"
-      class="bg-red-400 hover:bg-red-300 size-7"
-      @click="$emit('change-rating', 1)"
-    ></Button>
-    <Button
-      size="icon"
-      class="bg-orange-400 hover:bg-orange-300 size-7"
-      @click="$emit('change-rating', 2)"
-    ></Button>
-    <Button
-      size="icon"
-      class="bg-amber-300 hover:bg-amber-200 size-7"
-      @click="$emit('change-rating', 3)"
-    ></Button>
-    <Button
-      size="icon"
-      class="size-7"
-      @click="$emit('change-rating', 4)"
-    ></Button>
-    <Button
-      size="icon"
-      class="bg-gray-500 size-7 hover:bg-default"
-      @click="$emit('change-rating', -1)"
-    ></Button>
+    <BetterTooltip tooltip="Unknown">
+      <Button
+        size="icon"
+        class="bg-rose-600 hover:bg-rose-500 size-7"
+        @click="$emit('change-rating', 0)"
+      ></Button
+    ></BetterTooltip>
+    <BetterTooltip tooltip="Learning">
+      <Button
+        size="icon"
+        class="bg-red-400 hover:bg-red-300 size-7"
+        @click="$emit('change-rating', 1)"
+      ></Button
+    ></BetterTooltip>
+    <BetterTooltip tooltip="Recognized">
+      <Button
+        size="icon"
+        class="bg-orange-400 hover:bg-orange-300 size-7"
+        @click="$emit('change-rating', 2)"
+      ></Button>
+    </BetterTooltip>
+    <BetterTooltip tooltip="Familiar">
+      <Button
+        size="icon"
+        class="bg-amber-300 hover:bg-amber-200 size-7"
+        @click="$emit('change-rating', 3)"
+      ></Button>
+    </BetterTooltip>
+    <BetterTooltip tooltip="Known">
+      <Button
+        size="icon"
+        class="size-7"
+        @click="$emit('change-rating', 4)"
+      ></Button>
+    </BetterTooltip>
+    <BetterTooltip tooltip="Ignored">
+      <Button
+        size="icon"
+        class="bg-gray-500 size-7 hover:bg-default"
+        @click="$emit('change-rating', -1)"
+      ></Button>
+    </BetterTooltip>
   </div>
 </template>
