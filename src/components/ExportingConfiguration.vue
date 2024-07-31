d5797d4514a029803ab4382a585d3f735ffdfc43
Finn Brewer <weeaseled@gmail.com>
2024-07-31 14:28:30 -0700

    feat: add variable selector

--- src/components/ExportingConfiguration.vue
+++ src/components/ExportingConfiguration.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { computedAsync } from "@vueuse/core";
 import StyledCombobox from "@/components/StyledCombobox.vue";
-import { watch } from "vue";
+import { ref, watch } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
-import { Info } from "lucide-vue-next";
+import { CircleDot, Info } from "lucide-vue-next";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import { toast } from "vue-sonner";
 
 const props = defineProps<{
@@ -24,19 +34,33 @@ const fields = defineModel<{ [key: string]: string }>("fields", {
   required: true,
 });
 
-const fieldNames = computedAsync(
-  async () =>
-    await invoke<string[]>("get_note_field_names", {
-      model: model.value,
-    }).catch((error) => {
-      toast.error(error);
-    }),
-  [],
-);
+const openSelectors = ref<boolean[]>([]);
+
+const fieldNames = computedAsync(async () => {
+  const fields = await invoke<string[]>("get_note_field_names", {
+    model: model.value,
+  }).catch((error) => {
+    toast.error(error);
+  });
+  if (fields) {
+    openSelectors.value = new Array(fields.length).fill(false);
+  }
+  return fields;
+}, []);
 
 watch(model, async (_) => {
   fields.value = {};
 });
+
+function handleOpenChange(index: number) {
+  openSelectors.value[index] = !openSelectors.value[index];
+}
+
+const builtins = [
+  { name: "Word", value: "{word}" },
+  { name: "Sentence", value: "{sentence}" },
+  { name: "All definitions", value: "{def}" },
+];
 </script>
 
 <template>
@@ -78,6 +102,37 @@ watch(model, async (_) => {
   </HoverCard>
   <template v-for="(field, index) in fieldNames">
     <Label :for="index.toString()">{{ field }}</Label>
-    <Input :id="index.toString()" v-model="fields[field]" />
+    <div class="flex gap-2">
+      <Input class="w-96" :id="index.toString()" v-model="fields[field]" />
+      <Button variant="secondary" @click="handleOpenChange(index)"
+        ><CircleDot :size="16"
+      /></Button>
+      <CommandDialog
+        :open="openSelectors[index]"
+        @update:open="handleOpenChange(index)"
+      >
+        <Command>
+          <CommandInput class="h-9" placeholder="Search variables" />
+          <CommandEmpty>No variable found.</CommandEmpty>
+          <CommandList>
+            <CommandGroup heading="Builtins">
+              <CommandItem
+                v-for="builtin in builtins"
+                :value="builtin.value"
+                @select="
+                  if (fields[field]) {
+                    fields[field] += builtin.value;
+                  } else {
+                    fields[field] = builtin.value;
+                  }
+                  openSelectors[index] = false;
+                "
+                >{{ builtin.name }}</CommandItem
+              >
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </CommandDialog>
+    </div>
   </template>
 </template>
