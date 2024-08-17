1e6d8913f655d2fca95d6f7e5f276f310d1c19a0
Finn Brewer <weeaseled@gmail.com>
2024-08-17 12:03:31 -0700

    style: fix vue-tsc warnings

--- src/components/ui/chart/ChartLegend.vue
+++ src/components/ui/chart/ChartLegend.vue
@@ -1,50 +1,64 @@
 <script setup lang="ts">
-import { VisBulletLegend } from '@unovis/vue'
-import type { BulletLegendItemInterface } from '@unovis/ts'
-import { BulletLegend } from '@unovis/ts'
-import { nextTick, onMounted, ref } from 'vue'
-import { buttonVariants } from '@/components/ui/button'
+import { VisBulletLegend } from "@unovis/vue";
+import type { BulletLegendItemInterface } from "@unovis/ts";
+import { BulletLegend } from "@unovis/ts";
+import { nextTick, onMounted, ref } from "vue";
+import { buttonVariants } from "@/components/ui/button";
 
-const props = withDefaults(defineProps<{ items: BulletLegendItemInterface[] }>(), {
-  items: () => [],
-})
+const props = withDefaults(
+  defineProps<{ items: BulletLegendItemInterface[] }>(),
+  {
+    items: () => [],
+  },
+);
 
 const emits = defineEmits<{
-  'legendItemClick': [d: BulletLegendItemInterface, i: number]
-  'update:items': [payload: BulletLegendItemInterface[]]
-}>()
+  legendItemClick: [d: BulletLegendItemInterface, i: number];
+  "update:items": [payload: BulletLegendItemInterface[]];
+}>();
 
-const elRef = ref<HTMLElement>()
+const elRef = ref<HTMLElement>();
 
 onMounted(() => {
-  const selector = `.${BulletLegend.selectors.item}`
+  const selector = `.${BulletLegend.selectors.item}`;
   nextTick(() => {
-    const elements = elRef.value?.querySelectorAll(selector)
-    const classes = buttonVariants({ variant: 'ghost', size: 'xs' }).split(' ')
-    elements?.forEach(el => el.classList.add(...classes, '!inline-flex', '!mr-2'))
-  })
-})
+    const elements = elRef.value?.querySelectorAll(selector);
+    const classes = buttonVariants({
+      variant: "ghost",
+      size: "smallIcon",
+    }).split(" ");
+    elements?.forEach((el) =>
+      el.classList.add(...classes, "!inline-flex", "!mr-2"),
+    );
+  });
+});
 
 function onLegendItemClick(d: BulletLegendItemInterface, i: number) {
-  emits('legendItemClick', d, i)
-  const isBulletActive = !props.items[i].inactive
-  const isFilterApplied = props.items.some(i => i.inactive)
+  emits("legendItemClick", d, i);
+  const isBulletActive = !props.items[i].inactive;
+  const isFilterApplied = props.items.some((i) => i.inactive);
   if (isFilterApplied && isBulletActive) {
     // reset filter
-    emits('update:items', props.items.map(item => ({ ...item, inactive: false })))
-  }
-  else {
+    emits(
+      "update:items",
+      props.items.map((item) => ({ ...item, inactive: false })),
+    );
+  } else {
     // apply selection, set other item as inactive
-    emits('update:items', props.items.map(item => item.name === d.name ? ({ ...d, inactive: false }) : { ...item, inactive: true }))
+    emits(
+      "update:items",
+      props.items.map((item) =>
+        item.name === d.name
+          ? { ...d, inactive: false }
+          : { ...item, inactive: true },
+      ),
+    );
   }
 }
 </script>
 
 <template>
   <div ref="elRef" class="w-max">
-    <VisBulletLegend
-      :items="items"
-      :on-legend-item-click="onLegendItemClick"
-    />
+    <VisBulletLegend :items="items" :on-legend-item-click="onLegendItemClick" />
   </div>
 </template>
