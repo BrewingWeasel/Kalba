12a73db2a79f5f87a46c062333b1a5f29cbb2502
Finn Brewer <weeaseled@gmail.com>
2024-08-17 20:11:56 -0700

    fix: kalba not building on windows due to mutability problem

--- src-tauri/src/setup_stanza.rs
+++ src-tauri/src/setup_stanza.rs
@@ -16,17 +16,22 @@ pub async fn check_stanza_installed(state: State<'_, KalbaState>) -> Result<bool
     Ok(data_dir.exists() && !state.to_save.installing_stanza)
 }
 
+#[cfg(target_os = "windows")]
 fn new_command<S>(executable: S) -> Command
 where
     S: AsRef<std::ffi::OsStr>,
 {
-    let cmd = Command::new(executable);
-    #[cfg(windows)]
-    {
-        use std::os::windows::process::CommandExt;
-        cmd.creation_flags(0x08000000); // CREATE_NO_WINDOW constant
-    }
+    let mut cmd = Command::new(executable);
+    use std::os::windows::process::CommandExt;
+    cmd.creation_flags(0x08000000); // CREATE_NO_WINDOW constant
     cmd
+}
+#[cfg(not(target_os = "windows"))]
+fn new_command<S>(executable: S) -> Command
+where
+    S: AsRef<std::ffi::OsStr>,
+{
+    Command::new(executable)
 }
 
 #[tauri::command]
